c19623c8223cee7ba391bd8171025746ea0d5ba8
ZachCutler04 <zach.t.cutler@gmail.com>
2025-07-28 17:56:56 -0600

    trying webm

--- src/controllers/ComponentController.tsx
+++ src/controllers/ComponentController.tsx
@@ -93,7 +93,7 @@ export function ComponentController() {
       navigator.mediaDevices.getUserMedia({
         audio: true,
       }).then((s) => {
-        const recorder = new MediaRecorder(s);
+        const recorder = new MediaRecorder(s, { mimeType: 'audio/webm' });
         audioStream.current = recorder;
         audioStream.current.start();
         storeDispatch(setIsRecording(true));
